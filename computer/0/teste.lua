ed87d2161a8bbeb06ebe6f1e05084fcfd5b00300
Reidne Moreira <reidnemu@yahoo.com>
2026-04-27 18:50:37 -0300

    feat: ainda testando o arquivo lua

--- computer/0/teste.lua
+++ computer/0/teste.lua
@@ -1,3 +1,9 @@
-print("-" * 30)
-print("Bem-vindo ao Mundo Moderno")
-print("-" * 30)
+local function printCenter(text, width)
+    local pad = math.floor((width - #text) / 2)
+    print(string.rep(" ", pad) .. text)
+end
+
+local w, h = term.getSize() -- Pega o tamanho da tela do CC
+print(string.rep("=", w))
+printCenter("SISTEMA OPERACIONAL", w)
+print(string.rep("=", w))
